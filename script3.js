8abb87ff1a8fec5a0143b0ec14cc12ba4a9e71de
Ilya8788 <itsvey@gmail.com>
2024-01-03 21:57:03 +0200

    test

--- script3.js
+++ script3.js
@@ -1,9 +1,13 @@
-const arr = ["apple", "banana", "grapes", "lemon", "orange", "pineApple"];
+let arr = ["apple", "banana", "grapes", "lemon", "orange", "pineApple"];
 const fruits = document.querySelector(".fruits");
 const map = arr.map((x) => x.concat(".jpg"));
 const bottom = document.querySelector(".bottom");
-bottom.setAttribute("style", "height: 80px");
+// bottom.setAttribute("style", "height: 80px");
 fruits.setAttribute("style", "text-align: center");
+bottom.setAttribute("style", "text-align: center");
+let h3 = document.createElement("h3");
+h3.setAttribute("style", " font-size: 50px;");
+bottom.append(h3);
 
 // let new_el = document.createElement("img");
 
@@ -21,8 +25,39 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 const apple = document.querySelector(".apple");
+const banana = document.querySelector(".banana");
+const grapes = document.querySelector(".grapes");
+const lemon = document.querySelector(".lemon");
+const orange = document.querySelector(".orange");
+const pineApple = document.querySelector(".pineApple");
+
+// const apple = document.querySelector(".apple");
+
+// apple.addEventListener("click", function () {
+//   bottom.setAttribute("style", "text-align: center");
+//   bottom.textContent = "Apple";
+// });
 
 apple.addEventListener("click", function () {
-  bottom.setAttribute("style");
-  bottom.textContent = "Apple";
+  h3.innerText = arr[0];
+});
+
+banana.addEventListener("click", function () {
+  h3.innerText = arr[1];
+});
+
+grapes.addEventListener("click", function () {
+  h3.innerText = arr[2];
+});
+
+lemon.addEventListener("click", function () {
+  h3.innerText = arr[3];
+});
+
+orange.addEventListener("click", function () {
+  h3.innerText = arr[4];
+});
+
+pineApple.addEventListener("click", function () {
+  h3.innerText = arr[5];
 });
